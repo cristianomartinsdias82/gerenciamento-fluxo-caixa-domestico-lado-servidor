069c0e9b4c0095be617783ea1bb76ce8990fbbab
Cristiano Martins Dias <crisdi@gmail.com>
2026-01-15 17:12:42 -0300

    Add OpenApi (Scalar) to Api project

--- src/hosts/HouseholdCashflowManagerApi/Program.cs
+++ src/hosts/HouseholdCashflowManagerApi/Program.cs
@@ -1,4 +1,5 @@
 using IoC;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +9,23 @@ DependenciesContainer
 		builder.Services,
 		builder.Configuration);
 
+builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.MapOpenApi();
+	app.MapScalarApiReference(options =>
+	{
+		options.Title = "Household Cash Flow Management API Documentation";
+		options.ShowSidebar = true;
+		options.Theme = ScalarTheme.Laserwave;
+	});
+}
+
 app.UseAuthorization();
 
 app.MapControllers();
